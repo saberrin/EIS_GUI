7c6e5a66e9fb0d045a5146b04b1e784be7a2f0c4
Ren Jiang <brucejeaung@gmail.com>
2024-11-07 11:41:37 +0800

    stop reusing sqlite connections

--- database/repository.py
+++ database/repository.py
@@ -5,29 +5,24 @@ from datetime import datetime
 from config import DB_PATH
 
 class Repository:
-    def __init__(self):
-        self.connection = sqlite3.connect(DB_PATH)
-
-    def __del__(self):
-        if self.connection:
-            self.connection.close()
-
     def insert_measurements(self, measurements: List[EisMeasurement]):
         measurements = [(m.cell_id, int(m.creation_time.timestamp()), m.frequency, m.real_impedance, m.imag_impedance, m.voltage) for m in measurements]
         try:
-            cursor = self.connection.cursor()
+            connection = sqlite3.connect(DB_PATH)
+            cursor = connection.cursor()
             cursor.executemany("""
             INSERT INTO eis_measurement (cell_id, creation_time, frequency, real_impedance, imag_impedance, voltage)
             VALUES (?, ?, ?, ?, ?, ?);
             """, measurements)
-            self.connection.commit()
+            connection.commit()
         finally:
-            if cursor:
-                cursor.close()
+            if connection:
+                connection.close()
 
     def get_cluster_latest_measurements(self, cluster_id: int, limit: int = 50) -> dict[int, List[EisMeasurement]]:
         try:
-            cursor = self.connection.cursor()
+            connection = sqlite3.connect(DB_PATH)
+            cursor = connection.cursor()
             rows = cursor.execute("SELECT id from battery_cell WHERE cluster_id = ?;", (cluster_id,)).fetchall()
 
             cell_ids = [row[0] for row in rows]
@@ -65,15 +60,16 @@ class Repository:
                 result[cell_id].append(measurement)
             return result
         finally:
-            if cursor:
-                cursor.close()
+            if connection:
+                connection.close()
 
     def get_cell_measurements(self, cell_id: int, limit: int = 50) -> List[EisMeasurement]:
         try:
-            cursor = self.connection.cursor()
+            connection = sqlite3.connect(DB_PATH)
+            cursor = connection.cursor()
             cursor.execute("SELECT * FROM eis_measurement WHERE cell_id = ? ORDER BY creation_time DESC LIMIT ?;", (cell_id, limit))
             rows = cursor.fetchall()
             return [EisMeasurement(cell_id=row[1], creation_time=datetime.fromtimestamp(row[2]), frequency=row[3], real_impedance=row[4], imag_impedance=row[5], voltage=row[6]) for row in rows]
         finally:
-            if cursor:
-                cursor.close()
+            if connection:
+                connection.close()
